46de028c12504bb053f19d4bc4258933d98b812a
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-12 00:59:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,5 +4,15 @@ def using_concat(array_one, array_two)
 end
 
 def using_insert(array, element)
+  new_array = array.insert(element)
+  new_array
+end
+
+def using_uniq(array)
+  new_array = array.uniq
+  new_array
+end
+
+def using_flatten(array)
   
 end
